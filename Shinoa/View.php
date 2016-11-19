abd59f914762c98133ca469c4a5b5a7fdb586203
Al-faqun <inwronglocate@gmail.com>
2016-11-19 20:47:37 +0300

    Less php code in templates
    I am still in doubts about whether it is right to place any php code
    other than 'if's and 'for's in my templates. Generally people avoids
    this, but I think it's plausible, except that I try to make css for
    quotes run-time through php, which is obviously some work for JS... but
    I don't know JS ;0

--- Shinoa/View.php
+++ Shinoa/View.php
@@ -80,6 +80,68 @@ namespace Shinoa;
 				throw new DatabaseException('Template dir is not a valid directory!');
 			}
 		}
+		
+		/**
+		 * Procedure places ready css code of place and color for a text quote
+		 * 
+		 * @param Int $left_offset
+		 * @param Int $top_offset
+		 * @param String by reference $top_outcome
+		 * @param String by reference $left_outcome
+		 * @param String by reference $color_outcome
+		 */
+		public static function randPosAndColor($left_offset, $top_offset, 
+				                          &$left_outcome, &$top_outcome, &$color_outcome)
+		{
+			//определяем расположение по горизонтали
+			$num = $left_offset + mt_rand(0, 5);
+			$left_outcome = "left: $num" . '%';
+			//по вертикали
+			$num = $top_offset + mt_rand(0, 5);
+			$top_outcome = "top: $num" . '%'; 
+			//и цвет
+			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
+			$color_outcome = 'color: #' . "$num"; 
+		}
+		
+		/**
+		 * 
+		 * @return Array Array of tops, lefts and colors of every quote; 
+		 * use 'top', 'left' and 'color' as first index, number of quote as second index.
+		 */
+		public static function quotesPosAndColor($count) 
+		{
+			$quotesPosAndColor = array();
+			for ($i = 0; $i < $count; $i++) { 	//для каждой из выбранных цитат
+				$top = 0; $left = ''; $color = '';
+				switch ($i) {
+					case 0:
+						self::randPosAndColor(10, 10, $top, $left, $color);
+						break;
+					case 1:
+						self::randPosAndColor(50, 25, $top, $left, $color);
+						break;
+					case 2:
+						self::randPosAndColor(10, 40, $top, $left, $color);
+						break;
+					case 3:
+						self::randPosAndColor(50, 55, $top, $left, $color);
+						break;
+					case 4:
+						self::randPosAndColor(10, 70, $top, $left, $color);
+						break;
+					case 5:
+						self::randPosAndColor(50, 85, $top, $left, $color);
+						break;
+					default: 
+				}
+				$quotesPosAndColor['top'][$i] = $top;
+				$quotesPosAndColor['left'][$i] = $left; 
+				$quotesPosAndColor['color'][$i] = $color; 
+			}
+			return $quotesPosAndColor;
+		}
+		
 		/**
 		 * Возвращает текст нужной страницы.
 		 * @param string $page
@@ -91,7 +153,17 @@ namespace Shinoa;
 			ob_start();
 			switch ($page) {
 				case 'main':
-					//задаём переменные, необходимые в главно шаблоне
+					//задаём переменные, необходимые в главноv шаблоне
+					//необходимые данные из модели
+					try {
+						$color_on = $this->model->getColor();
+						$changes = $this->model->getChanges();
+						$citations = $this->model->getQuotes();
+						$count = $this->model->getNumOfQuotes();
+					} catch (ModelException $e) {
+						throw new ViewException('Cannot output: ' . $e->getMessage());
+					}
+					
 					//заголовок страницы 
 					$header = $this->doc_root . '/Phoenix_demo/Private/tpl_header.php'; 
 					if (!file_exists($header) || !is_file($header)) {
@@ -116,15 +188,8 @@ namespace Shinoa;
 						throw new ViewException('Cannot output: citations template not found.');
 					}
 					
-					//необходимые данные из модели
-					try {
-						$color_on = $this->model->getColor();
-						$changes = $this->model->getChanges();
-						$citations = $this->model->getQuotes();
-						$count = $this->model->getNumOfQuotes();
-					} catch (ModelException $e) {
-						throw new ViewException('Cannot output: ' . $e->getMessage());
-					}
+					//css для tpl_citations
+					$quotesPosAndColor = self::quotesPosAndColor($count);
 					
 					//вызывает общий шаблон страницы
 					$outputFile = $this->templateDir . '/tpl_main.php';
--- templates/tpl_citations.php
+++ templates/tpl_citations.php
@@ -1,76 +1,17 @@
 <!-- скрипт получает текст цитат и публикует его на сайте -->
+<?php 
+	//проверка, все ли переменные заданы перед запуском шаблона
+	if (!isset($quotesPosAndColor))
+		throw new Exception('No_var'); 
+?>
 
 <?php for ($i = 0; $i < $count; $i++): 	//для каждой из выбранных цитат ?>
-<?php switch ($i): ?>
-<?php		case 0:
-			//определяем расположение по горизонтали
-			$num = 10 + mt_rand(0, 5);
-			$left = "left: $num" . '%';
-			//по вертикали
-			$num = 10 + mt_rand(0, 5);
-			$top = "top: $num" . '%'; 
-			//и цвет
-			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
-			$color = 'color: #' . "$num"; 
-			break;
-		case 1:
-			$num = 10 + mt_rand(0, 5);
-			$left = "right: $num" . '%';
-			
-			$num = 25 + mt_rand(0, 5);
-			$top = "top: $num" . '%'; 
-			
-			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
-			$color = 'color: #' . "$num"; 
-			break;
-		case 2:
-			$num = 10 + mt_rand(0, 5);
-			$left = "left: $num" . '%';
-			
-			$num = 40 + mt_rand(0, 5);
-			$top = "top: $num" . '%'; 	
-			
-			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
-			$color = 'color: #' . "$num"; 
-			break;
-		case 3:
-			$num = 10 + mt_rand(0, 5);
-			$left = "right: $num" . '%';
-			
-			$num = 55 + mt_rand(0, 5);
-			$top = "top: $num" . '%'; 
-			
-			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
-			$color = 'color: #' . "$num"; 
-			break;
-		case 4:
-			$num = 10 + mt_rand(0, 5);
-			$left = "left: $num" . '%';
-			
-			$num = 70 + mt_rand(0, 5);
-			$top = "top: $num" . '%'; 
-			
-			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
-			$color = 'color: #' . "$num"; 
-			break;
-		case 5:
-			$num = 10 + mt_rand(0, 5);
-			$left = "right: $num" . '%';
-			
-			$num = 85 + mt_rand(0, 5);
-			$top = "top: $num" . '%';
-			
-			$num = dechex(mt_rand(hexdec('000000'), hexdec('ffffff')));
-			$color = 'color: #' . "$num"; 
-			break;
-		default: ?>
-<?php endswitch; ?>
-	
 	<!-- постим цитату с нужными стилями -->
 	<div style="position: absolute; 
-					<?php echo $top; ?>;
-					<?php echo $left; ?>;
-					<?php if ($color_on === true) echo $color; ?>;">
+			<?php echo $quotesPosAndColor['top'][$i]; ?>;
+			<?php echo $quotesPosAndColor['left'][$i]; ?>;
+			<?php if ($color_on === true) echo $quotesPosAndColor['color'][$i]; ?>;
+			">
 		<p>«<?php echo htmlspecialchars($citations[$i], ENT_QUOTES, 'UTF-8'); ?>»</p>
 	</div>
 <?php endfor; ?>
